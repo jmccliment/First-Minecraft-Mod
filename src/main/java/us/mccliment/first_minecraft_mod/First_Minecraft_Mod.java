757e1f86315af6395c732dd8a5e07dee81b42a0a
Jeremy McCliment <jmccliment@gmail.com>
2015-07-12 13:52:52 -0700

    Added Mod event handlers

--- src/main/java/us/mccliment/first_minecraft_mod/First_Minecraft_Mod.java
+++ src/main/java/us/mccliment/first_minecraft_mod/First_Minecraft_Mod.java
@@ -6,4 +6,8 @@ import us.mccliment.first_minecraft_mod.help.*;
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
 public class First_Minecraft_Mod {
 
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
 }
