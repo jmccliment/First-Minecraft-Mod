8d48d2e4faa9bd323edb910fba27affb96c3c482
Jeremy McCliment <jmccliment@gmail.com>
2015-07-12 13:48:47 -0700

    initial setup of mod and reference classes

---
+++ src/main/java/us/mccliment/first_minecraft_mod/First_Minecraft_Mod.java
@@ -0,0 +1,9 @@
+package us.mccliment.first_minecraft_mod;
+
+import net.minecraftforge.fml.common.Mod;
+import us.mccliment.first_minecraft_mod.help.*;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class First_Minecraft_Mod {
+
+}
---
+++ src/main/java/us/mccliment/first_minecraft_mod/help/Reference.java
@@ -0,0 +1,7 @@
+package us.mccliment.first_minecraft_mod.help;
+
+public class Reference {
+	public static final String MODID = "first_minecraft_mod";
+	public static final String NAME = "First Minecraft Mod";
+	public static final String VERSION = "a4.2.283c";
+}
